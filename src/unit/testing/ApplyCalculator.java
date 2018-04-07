b0dda8060c026938034db91aec7ee8c9d5c8ea96
Selina Tanha <tanha.selina75@gmail.com>
2018-04-07 14:31:38 -0400

    Add the file

---
+++ src/unit/testing/ApplyCalculator.java
@@ -0,0 +1,16 @@
+package unit.testing;
+
+import org.testng.Assert;
+
+public class ApplyCalculator {
+    public static void main(String[] args){
+
+        Calculator cal = new Calculator();
+        Assert.assertEquals(cal.addition(10,15),  25);
+        Assert.assertEquals(cal.subtraction(20,10), 10);
+
+        Assert.assertEquals(cal.multiplication(10,10), 100);
+    }
+
+
+}
---
+++ src/unit/testing/Calculator.java
@@ -0,0 +1,21 @@
+package unit.testing;
+
+
+
+public class Calculator {
+    public int addition(int num1, int num2){
+        int total = 10 + 15;
+        return total;
+    }
+    public int subtraction(int num1, int num2){
+        int total = 20 - 10;
+        return total;
+    }
+
+    public int multiplication(int num1, int num2){
+        int total = 10 * 10;
+        return total;
+    }
+
+}
+
